42a4d3dc1c2b2e4c58b1084b4cbd12d9c3ffc65f
danieltheodosius <danielt@intersect.org.au>
2013-09-04 10:03:44 +1000

    DEVSUPPORT-1089 - gemify cap task for creating deployment record

---
+++ lib/create_deployment_record.rb
@@ -0,0 +1,43 @@
+unless Capistrano::Configuration.respond_to?(:instance)
+    abort "capistrano/shared_files requires Capistrano 2"
+end
+
+if Capistrano::Configuration.instance
+  Capistrano::Configuration.instance.load do
+
+    task :create_deployment_record do
+      require 'net/http'
+      require 'socket'
+
+      branchName = "HEAD"
+      availableTags = `git tag`.split( /\r?\n/ )
+      haveToShowHash = !availableTags.any? { |s| s.include?(branchName) }
+      current_deployed_version = branchName
+      if (haveToShowHash)
+        availableBranches = `git branch -a`.split( /\r?\n/ )
+        fullBranchName = (branchName.eql?("HEAD")) ? branchName : availableBranches.select { |s| s.include?(branchName) }.first.to_s.strip
+        fullBranchName.gsub!('*','').strip! if fullBranchName.include?('*')
+        current_deployed_version += " (sha1:" + `git rev-parse --short #{fullBranchName}`.strip + ")"
+      end
+
+      url = URI.parse('http://deployment-tracker.intersect.org.au/deployments/api_create')
+      post_args = {'app_name'=>application,
+                   'deployer_machine'=>"#{ENV['USER']}@#{Socket.gethostname}",
+                   'environment'=>rails_env,
+                   'server_url'=>find_servers[0].to_s,
+                   'tag'=> current_deployed_version}
+      begin
+        print "Sending Post request with args: #{post_args}\n"
+        resp, data = Net::HTTP.post_form(url, post_args)
+        case resp
+          when Net::HTTPSuccess
+            puts "Deployment record saved"
+          else
+            puts data
+        end
+      rescue StandardError => e
+        puts e.message
+      end
+    end
+  end
+end
